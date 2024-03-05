85c8b10d8f3c344e36fa19a0d9d5f87ec956b9af
BR <244097@niuitmo.ru>
2024-03-05 15:44:03 +0300

    Добавьте файлы проекта.

---
+++ Задача/Linear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача
+{
+    struct Linear
+    {
+        private double k;
+        private double b;
+
+        public Linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            if (k == 0)
+            {
+                return "Неверный формат уравнения";
+            }
+            else
+            {
+                double r = -b / k;
+                return $" x = {r}";
+            }
+        }
+    }
+}
---
+++ Задача/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Значение k: ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Значение b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Linear linear = new Linear(k,b) ;
+            Console.WriteLine(linear.Root());
+            Console.ReadKey();
+        }
+    }
+}
